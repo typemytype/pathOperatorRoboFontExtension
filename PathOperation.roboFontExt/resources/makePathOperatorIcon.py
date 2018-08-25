f98b26df999de45ce1754badec265e969b480a96
Gustavo Ferreira <gustavo@hipertipo.com>
2018-08-25 07:28:55 -0300

    adding resources folder with icon

---
+++ PathOperation.roboFontExt/resources/makePathOperatorIcon.py
@@ -0,0 +1,19 @@
+size(500, 500)
+fill(1)
+rect(0, 0, width(), height())
+
+B1 = BezierPath()
+B1.rect(60, 50, 250, 250)
+
+B2 = BezierPath()
+B2.oval(160, 160, 290, 290)
+
+B3 = B1.xor(B2)
+
+fill(0)
+drawPath(B3)
+
+import os
+folder = os.getcwd()
+imgPath = os.path.join(folder, 'pathOperatorIcon.png')
+saveImage(imgPath)
